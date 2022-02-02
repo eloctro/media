5af9cabaa823541b60f3a3b939e1a8112fb23328
eloctro <eloctro@tut.by>
2022-02-02 09:36:39 +0300

    audio-3

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const audio = document.querySelector("audio");
+// const audio = document.querySelector("audio");
 const playBtn = document.querySelector(".play-btn");
 const pauseBtn = document.querySelector(".pause-btn");
 
+const audio = new Audio();
+
 function playAudio() {
+  audio.src = "./assets/audio/01-odinokiy-pastu-01-odinokiy-pastuh.mp3";
   audio.currntTime = 0;
   audio.play();
 }
