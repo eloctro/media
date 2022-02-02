43f13fe65cb10e3c1beacf1e2a1beb3454463a01
eloctro <eloctro@tut.by>
2022-02-02 10:13:53 +0300

    play stop

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // const audio = document.querySelector("audio");
 const playBtn = document.querySelector(".play-btn");
 const pauseBtn = document.querySelector(".pause-btn");
-
 const audio = new Audio();
+let isPlay = false;
 
 function playAudio() {
-  audio.src = "./assets/audio/01-odinokiy-pastu-01-odinokiy-pastuh.mp3";
-  audio.currntTime = 0;
-  audio.play();
+  if (!isPlay) {
+    audio.src = "./assets/audio/01-odinokiy-pastu-01-odinokiy-pastuh.mp3";
+    audio.currntTime = 0;
+    audio.play();
+    isPlay = true;
+  } else {
+    pauseAudio();
+  }
 }
 
 function pauseAudio() {
   audio.pause();
+  isPlay = false;
 }
 
 playBtn.addEventListener("click", playAudio);
